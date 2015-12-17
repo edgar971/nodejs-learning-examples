6be764aba4906b068f77384f31fcae3a2b796961
Edgar Pino <edgarbeltran@me.com>
2015-12-16 23:41:15 -0600

    decoupled data and server

--- examples/airline/app.js
+++ examples/airline/app.js
@@ -1,67 +1,39 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var exphbs = require('express-handlebars');
+
+module.exports = function(flightsData) {
+    var express = require('express');
+    var path = require('path');
+    var favicon = require('serve-favicon');
+    var logger = require('morgan');
+    var cookieParser = require('cookie-parser');
+    var bodyParser = require('body-parser');
+    var exphbs = require('express-handlebars');
 
 
-var routes = require('./routes/index');
+    var routes = require('./routes/index')(flightsData);
 
-var app = express();
+    var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.engine('.hbs', exphbs({defaultLayout: 'single', extname:'.hbs'}));
-app.set('view engine', '.hbs');
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static('./node_modules'));
-/**
- * Middleware
- */
-app.use(function (req,res,next) {
-    res.set('X-Powered-By', 'Awesome Server');
-    next();
-});
-app.use('/', routes);
+    // view engine setup
+    app.set('views', path.join(__dirname, 'views'));
+    app.engine('.hbs', exphbs({defaultLayout: 'single', extname:'.hbs'}));
+    app.set('view engine', '.hbs');
+    // uncomment after placing your favicon in /public
+    //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+    app.use(logger('dev'));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(cookieParser());
+    app.use(express.static(path.join(__dirname, 'public')));
+    app.use(express.static('./node_modules'));
 
-// catch 404 and forward to error handler
-//app.use(function(req, res, next) {
-//  var err = new Error('Not Found');
-//  err.status = 404;
-//  next(err);
-//});
+    /**
+     * Middleware
+     */
+    app.use(function (req,res,next) {
+        res.set('X-Powered-By', 'Awesome Server');
+        next();
+    });
+    app.use('/', routes);
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-//if (app.get('env') === 'development') {
-//  app.use(function(err, req, res, next) {
-//    res.status(err.status || 500);
-//    res.render('error', {
-//      message: err.message,
-//      error: err
-//    });
-//  });
-//}
-//
-//// production error handler
-//// no stacktraces leaked to user
-//app.use(function(err, req, res, next) {
-//  res.status(err.status || 500);
-//  res.render('error', {
-//    message: err.message,
-//    error: {}
-//  });
-//});
-
-
-module.exports = app;
+    return app;
+};
--- examples/airline/routes/index.js
+++ examples/airline/routes/index.js
@@ -1,39 +1,50 @@
-var express = require('express'),
-    router = express.Router(),
-    flight = require('../../../custom_modules/flight'),
-    flightsData = require('../data'),
-    flights = {};
+
+module.exports = function(flightsData){
+    var express = require('express'),
+        router = express.Router(),
+        flight = require('../../../custom_modules/flight'),
+        flights = {};
 
 
-flightsData.forEach(function(item,index,array){
-    flights[item.number] = flight.create(item);
-});
-router.get('/api/', function(req, res, next) {
-  res.json(flightsData);
-});
+    flightsData.forEach(function(item,index,array){
+        flights[item.number] = flight.create(item);
+    });
 
-router.get('/api/flight/:number', function(req, res, next){
-    var number = req.param('number');
-    if(typeof flights[number] !== 'undefined') {
-        res.json(flights[number].getInformation());
-    } else {
-        res.json({status: 'error'});
-    }
 
-});
+    /**
+     * API
+     */
+    router.get('/api/', function(req, res, next) {
+        res.json(flightsData);
+    });
 
-router.put('/api/flight/:number/arrived', function(req, res){
-    var number = req.param('number');
-    if(typeof flights[number] !== 'undefined') {
-        flights[number].triggerArrive();
-        res.json({status: 'success'});
+    router.get('/api/flight/:number', function(req, res, next){
+        var number = req.param('number');
+        if(typeof flights[number] !== 'undefined') {
+            res.json(flights[number].getInformation());
+        } else {
+            res.json({status: 'error'});
+        }
 
-    } else {
-        res.json({status: 'error'});
-    }
-});
+    });
 
-router.get('/', function(req,res){
-    res.render('index', {flights: flightsData});
-});
-module.exports = router;
+    router.put('/api/flight/:number/arrived', function(req, res){
+        var number = req.param('number');
+        if(typeof flights[number] !== 'undefined') {
+            flights[number].triggerArrive();
+            res.json({status: 'success'});
+
+        } else {
+            res.json({status: 'error'});
+        }
+    });
+
+    /**
+     * HTML Rendering
+     */
+    router.get('/', function(req,res){
+        res.render('index', {flights: flightsData});
+    });
+
+    return router;
+};
