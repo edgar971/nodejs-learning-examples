1c2f2a00ba9e13778dd10c28958958ba7c522877
Edgar Pino <edgarbeltran@me.com>
2015-12-16 23:58:23 -0600

    added test examples

--- examples/airline/tests/FlightSpec.js
+++ examples/airline/tests/FlightSpec.js
@@ -1,1 +1,13 @@
-var app = require('./helpers/app');
+var app = require('./helpers/app'),
+    should = require('should'),
+    supertest = require('supertest');
+
+describe('flights', function(){
+    it('should pass', function(done){
+        done();
+    });
+    it('should not pass', function(done){
+        throw "don't pass";
+        done();
+    });
+})
