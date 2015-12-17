eaf99ef246e0fe595f85555206e09f618642ab7d
Edgar Pino <edgarbeltran@me.com>
2015-12-16 23:51:46 -0600

    added tests

---
+++ examples/airline/tests/data/index.js
@@ -0,0 +1,23 @@
+module.exports = [
+    {
+        "number": "765122",
+        "origin": "OK",
+        "destination": "SD",
+        "departs": 5,
+        "arrives": 6
+    },
+    {
+        "number": "677225",
+        "origin": "DE",
+        "destination": "KS",
+        "departs": 5,
+        "arrives": 8
+    },
+    {
+        "number": "299454",
+        "origin": "MO",
+        "destination": "VA",
+        "departs": 11,
+        "arrives": 2
+    }
+];
